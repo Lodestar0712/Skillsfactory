9f64d339c77ee03b4455192b19eb9dff4579c49c
Lodestar0712 <102283055+Lodestar0712@users.noreply.github.com>
2022-05-09 09:29:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5330215960:AAGNLpxykcboJ-Autl9E2ykNk5dFcGxsHgY"
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+}
